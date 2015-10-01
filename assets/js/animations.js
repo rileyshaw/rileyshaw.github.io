bc86bf8cfa93d57995abf492f32162a1578b0c3d
Riley SHaw <shaw65@purdue.edu>
2015-09-30 21:25:05 -0400

    fixing button scroll thing ayy

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,6 +1,28 @@
+var projects = [
+{projectName: "project1",linkToProject: "rileyshaw.net/project1.html"},
+{projectName: "project2",linkToProject: "rileyshaw.net/project2.html"},
+{projectName: "project3",linkToProject: "rileyshaw.net/project3.html"}
+]
+var curpage = 0;
+function nextPage(){
+	if(curpage+1 >= projects.length){
+		curpage = 0;
+	}else{
+		curpage++;
+	}
+}
+function prevPage(){
+	if(curpage == 0){
+		curpage = projects.length - 1;
+	}else{
+		curpage--;
+	}
+}
 $(document).ready(function() {
-	$("#selector").disableScroll();
-	$("#down_button").click(function(){
+	$(".down_button").click(function(){
+		$( ".mainbody" ).toggle("slide", { direction: "up" }, 500);
+	});
+	$(".up_button").click(function(){
 		$( ".main-header" ).toggle("slide", { direction: "up" }, 500);
 	});
 
